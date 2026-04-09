03c6cf9617461b2357f26cbf2cbbc2d6040ff666
Dunjinmaa Jamsran <dunjinmaa@gmail.com>
2026-04-09 10:25:21 +0100

    trying to fix linter error

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
